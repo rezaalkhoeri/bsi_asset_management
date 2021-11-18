001409bc1cb962c7120e446b1840e48e333cad98
Reza <rezaalkhoeri2403@gmail.com>
2021-11-18 14:20:57 +0700

    Web Template & login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,5 +1,5 @@
   <!-- <div class="jumbotron big-banner" style="height:500px; padding-top: 150px;"> -->
-  
+
   <div class="container">
 
     <!-- Outer Row -->
@@ -14,34 +14,35 @@
             <!-- Nested Row within Card Body -->
             <div class="row">
               <!-- <div class="col-lg-6 d-none d-lg-block"></div> -->
-              <!-- <img class="col-lg-6 d-none d-lg-block" src="<?= base_url('assets/img/loginscreen.jpg');?>"> -->
-              <img class="col-lg-6 d-none d-lg-block animated--grow-in" src="<?= base_url('assets/img/logologin2.png');?>">
+              <!-- <img class="col-lg-6 d-none d-lg-block" src="<?= base_url('assets/img/logo_bsi_baru.png'); ?>"> -->
+              <img class="col-lg-6 d-none d-lg-block animated--grow-in" src="<?= base_url('assets/img/logo_bsi_baru.png'); ?>">
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4 font-weight-bold">Login</h1>
+                    <h1 class="h4 text-gray-900 mb-4 font-weight-bold">Asset Management</h1>
                   </div>
 
-                    <?= $this->session->flashdata('message')?>
-                  
+                  <?= $this->session->flashdata('message') ?>
+
                   <form class="user" method="post" action="<?= base_url('auth/index'); ?>">
-                    
+
                     <!-- input email -->
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user font-weight-bold" id="email" name="email" placeholder="Email" value="<?= set_value('email');?>">
-                      <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?> 
+                      <input type="text" class="form-control form-control-user font-weight-bold" id="email" name="email" placeholder="Email" value="<?= set_value('email'); ?>">
+                      <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
 
                     <!-- input password -->
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user font-weight-bold" id="password" name="password" placeholder="Password">
-                      <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?> 
+                      <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
 
                     <button type="submit" class="btn btn-success btn-user btn-block font-weight-bold">
                       Login
                     </button>
-                    </form>
+                  </form>
                   <hr>
                   <!-- <div class="text-center">
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
@@ -60,6 +61,4 @@
     </div>
 
   </div>
-<!-- </div> -->
-
-  
+  <!-- </div> -->
